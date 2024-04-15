702b84e77e44e5c64195749aea75a77ae6784b7d
AliMahmoudi-dev <ali.mahmoudi.dev.ir@gmail.com>
2024-04-15 14:29:45 +0330

    add schedule for payment

--- routes/console.php
+++ routes/console.php
@@ -8,3 +8,14 @@ use Illuminate\Support\Facades\Schedule;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::call(function () {
+    $transaction = resolve(App\Services\Payment\Transaction::class);
+    $unpaidInvoices = Invoice::where('status', 1)->get();
+
+    if ($unpaidInvoices->isNotEmpty()) {
+        foreach ($unpaidInvoices as $invoice) {
+            $transaction->pay($invoice);
+        };
+    };
+})->daily();
